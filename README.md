0e8e399b899d0fca09698b3f396ad8c6f3621194
Bener <2388697970@qq.com>
2018-09-24 11:26:06 +0800

    init gh-pages branch

--- README.md
+++
@@ -1,9 +0,0 @@
-# hello nav
-
-## TODO
-
-- [x] fix all image file
-- [ ] create nav categary & blur toggle(list & categary)
-- [ ] github link
-- [ ] grid layout
-- [ ] add sw
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import initIconfont from '../src/assets/fonts/iconfont'
-import './index.css'
-import App from './components/App'
-import registerServiceWorker from './registerServiceWorker'
-initIconfont()
-ReactDOM.render(<App />, document.getElementById('app'))
-registerServiceWorker()
